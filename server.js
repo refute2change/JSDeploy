12234f683ac1f0abaeec6aaa42c781af1618fa8e
refute2change <madguyinthehouse@gmail.com>
2025-05-22 09:03:25 +0700

    add dotenv dependencies

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('dotenv').config();
+
+console.log(process.env); // "239482"
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
